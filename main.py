0d4b488cb2de300b858608aab7c207d4f9b86ba7
talishagriesbach <talishagriesbach@gmail.com>
2020-05-18 06:26:28 +0100

    Day 1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+import seaborn
 
+msft = yf.Ticker("MSFT")
+print(msft.info)
+
+hist = msft.history(period="5d")
+print(hist)
+print(type(hist))
